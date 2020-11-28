36bad0e59d9193efd4aedb1a3d18f0839068c578
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-11-28 11:31:31 +0530

    Updates

---
+++ precache-manifest.e9c45dffde854fb63c7665bf20017adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2d83af70b6dbb7da32270cb3396bd0",
+    "url": "/lorem-ipsum/index.html"
+  },
+  {
+    "revision": "a75bef70ef43f7f9c904",
+    "url": "/lorem-ipsum/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/lorem-ipsum/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lorem-ipsum/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75bef70ef43f7f9c904",
+    "url": "/lorem-ipsum/static/js/main.7a8f7b04.chunk.js"
+  },
+  {
+    "revision": "c4ba6ab66839d64706a0",
+    "url": "/lorem-ipsum/static/js/runtime-main.1cd389ed.js"
+  }
+]);
